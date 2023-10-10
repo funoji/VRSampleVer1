f7033c4fb31efaa41930e895b04d17c0f219f975
kusomanju <g2127072@st.t-kougei.ac.jp>
2023-10-10 11:22:23 +0900

    爆弾を作成しました
    まだ作成中。エフェクトなども。

---
+++ Assets/Script/Gimmick/Explosion_Bomb_SC.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_Bomb_SC : MonoBehaviour
+{
+    [Header("判定用子オブジェクト(BombArea)")]
+    [SerializeField] private GameObject BombArea;
+    [Header("爆発させるまでの時間")]
+    public float delayTime = 3000.0f;
+    [Header("爆発範囲(半径)")]
+    public float Ex_Radius = 5.0f;
+    [Header("爆発の威力")]
+    public float Ex_power = 20f;
+    [Header("上方向への力")]
+    public float Ex_powerUP = 5f;
+    [Header("爆発エフェクト")]
+    [SerializeField] private ParticleSystem Ex_particle;
+    [Header("爆発を阻害する障害物のレイヤー")]
+    public int Wall_Layer;
+
+    private bool IsExplosion = false;
+    private SphereCollider _SphereColl;
+
+    void Start()
+    {
+        _SphereColl = BombArea.GetComponent<SphereCollider>();
+        _SphereColl.radius = Ex_Radius;
+        BombArea.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        //爆発するまでの時間を待ち、エリア内のタグ付きオブジェクトを破壊
+        if (BombArea.gameObject.activeSelf)
+        {
+            StartCoroutine("ExplosionCoroutine");
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Bullet")  //当たったら爆発開始するオブジェクトのタグ
+        {
+            BombArea.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (IsExplosion && other.gameObject.tag == "CanExplosion")  //爆発判定を付与するオブジェクトのタグ
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private IEnumerator ExplosionCoroutine()
+    {
+        yield return new WaitForSeconds(delayTime * Time.deltaTime);
+        IsExplosion = true;
+    }
+
+    private void Explosion()
+    {
+        Ex_particle.transform.position = BombArea.transform.position;
+        Ex_particle.Play();
+
+        Collider[] HitColliders = Physics.OverlapSphere(BombArea.transform.position, Ex_Radius);
+        for (int i = 0; i < HitColliders.Length; i++)
+        {
+            var rb = HitColliders[i].GetComponent<Rigidbody>();
+            if (rb)
+            {
+                // Rayを飛ばして壁を挟んでいたら無視する
+                var hits = Physics.RaycastAll(BombArea.transform.position, HitColliders[i].transform.position - BombArea.transform.position, Ex_Radius);
+                bool block = false;
+                foreach (var o in hits)
+                {
+                    if (o.collider.gameObject.layer == Wall_Layer)
+                    {
+                        block = true;
+                        break;
+                    }
+                }
+                if (!block)
+                {
+                    rb.AddExplosionForce(Ex_power, BombArea.transform.position, Ex_Radius, Ex_powerUP, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
