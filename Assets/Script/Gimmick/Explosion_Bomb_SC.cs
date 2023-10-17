b6698507a9c8efb31b74448f0c30645fa5420c8d
kusomanju <g2127072@st.t-kougei.ac.jp>
2023-10-17 11:07:46 +0900

    爆発物の作成

--- Assets/Script/Gimmick/Explosion_Bomb_SC.cs
+++ Assets/Script/Gimmick/Explosion_Bomb_SC.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Explosion_Bomb_SC : MonoBehaviour
 {
+    [System.Serializable]
+    public class WaitClass
+    {
+        [Header("爆発までの時間(n秒)")]
+        public int delayTime = 3;
+    }
+
     [Header("判定用子オブジェクト(BombArea)")]
     [SerializeField] private GameObject BombArea;
-    [Header("爆発させるまでの時間")]
-    public float delayTime = 3000.0f;
     [Header("爆発範囲(半径)")]
     public float Ex_Radius = 5.0f;
     [Header("爆発の威力")]
-    public float Ex_power = 20f;
+    public float Ex_power = 20.0f;
     [Header("上方向への力")]
-    public float Ex_powerUP = 5f;
+    public float Ex_powerUP = 5.0f;
     [Header("爆発エフェクト")]
     [SerializeField] private ParticleSystem Ex_particle;
     [Header("爆発を阻害する障害物のレイヤー")]
     public int Wall_Layer;
+
+    [Header("爆発までの時間を設定したい場合")]
+    public bool Set_WaitTime = true;
+    [FlagConditionalDisableInInspector("Set_WaitTime")]
+    public WaitClass _waitclass;
 
     private bool IsExplosion = false;
     private SphereCollider _SphereColl;
@@ -36,6 +48,11 @@ public class Explosion_Bomb_SC : MonoBehaviour
         {
             StartCoroutine("ExplosionCoroutine");
         }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Bomb");
+            Explosion();
+        }
     }
 
     private void OnCollisionEnter(Collision col)
@@ -50,14 +67,26 @@ public class Explosion_Bomb_SC : MonoBehaviour
     {
         if (IsExplosion && other.gameObject.tag == "CanExplosion")  //爆発判定を付与するオブジェクトのタグ
         {
+            Explosion();
+            StartCoroutine("WaitTime");
             Destroy(other.gameObject);
         }
     }
 
     private IEnumerator ExplosionCoroutine()
     {
-        yield return new WaitForSeconds(delayTime * Time.deltaTime);
+        if (Set_WaitTime)
+        {
+            for (int waitCount = _waitclass.delayTime; waitCount > 0; waitCount--)
+            {
+                yield return new WaitForSeconds(1000f * Time.deltaTime);
+            }
+        }
         IsExplosion = true;
+    }
+    private IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(_waitclass.delayTime * Time.deltaTime);
     }
 
     private void Explosion()
